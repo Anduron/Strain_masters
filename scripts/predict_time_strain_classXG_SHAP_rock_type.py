e365c73fbee79a87e7ae544d4e180861336e76a0
Anduron <stig-nicolai@hotmail.com>
2020-09-20 19:35:23 +0200

    Larger run + figures

--- scripts/predict_time_strain_classXG_SHAP_rock_type.py
+++ scripts/predict_time_strain_classXG_SHAP_rock_type.py
@@ -8,7 +8,7 @@ Created on Wed Sep 25 10:51:45 2019
 
 import pandas as pd
 import numpy as np
- 
+
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
@@ -16,11 +16,11 @@ from sklearn.preprocessing import RobustScaler
 import shap
 import statistics
 
-folder = '~/Dropbox/HADES/TomoRun/matlab_run/txts/'
+folder = '../strain_data_MS/'
 np.random.seed(0)
 
 # get importance of features or not
-get_imp = 1 
+get_imp = 1
 # experiments
 #exps = ['FBL01', 'FBL02'] #, 'ETNA01', 'ETNA02']
 # two rocks per experiment
@@ -48,11 +48,11 @@ vals = ['p90', 'p75', 'p50', 'mean', 'p25', 'p10', 'std', 'num', 'sum']
 
 # potential predictisons
 #pred_str = 'distep'
-pred_str = 'distsigD2' 
+pred_str = 'distsigD2'
 
 rad = '5'
 clnum = 2 # 2 or 4
-accur_txt = 'txts/predict_strain_comb_'+pred_str+'_micro_accurs_r'+rad+'_XGc'+str(clnum)+feat_str+'.txt'
+accur_txt = '../txts/predict_strain_comb_'+pred_str+'_micro_accurs_r'+rad+'_XGc'+str(clnum)+feat_str+'.txt'
 print(accur_txt)
 accur_str = "exp1 exp2 time_classes accur rand_chance prec_cl0 rec_cl0 prec_cl1 rec_cl1 prec_cl2 rec_cl2 prec_cl3 rec_cl3\n"
 
@@ -73,28 +73,28 @@ for exp in exps:
             ei2max = ei2
             # if the first two letters of the exp name are the same
             filen = 'strains_curr_'+exp2+'_g'+rad+'0.txt'
-    
-            
+
+
             df = pd.read_csv(folder+filen, delim_whitespace=True)
             df = df.dropna().copy()
-            
+
             # make classes of time
             time_str = pred_str.replace('dist', '')
             times = df[time_str].values
-            
+
             normtime = [100*round((max(times)-t)/max(times), 2) for t in times]
-            
-            
+
+
             # divide times into this many bins
             timebins = [0, 50, 100]
             if clnum==4:
                 timebins = [0, 25, 50, 75, 100]
-                
+
             norms = set(normtime)
             outcomes = []
             oi=0
             for normt in normtime:
-                
+
                 outcome = max(timebins)
                 ti=0
                 while ti<len(timebins)-1:
@@ -102,35 +102,35 @@ for exp in exps:
                     tim2 = timebins[ti+1]
                     if normt < tim2 and normt >= tim1:
                         outcome = int(tim1)
-                        ti=len(timebins)   # exit the loop  
-                    
+                        ti=len(timebins)   # exit the loop
+
                     ti=ti+1
-                    
+
                 outcomes.append(outcome)
                 oi=oi+1
-                
-                
+
+
             # predict these classes of outcomes
             df[pred_str] = outcomes
             df_big = df_big.append(df, ignore_index = True)
-            
+
         ei2=ei2+1
-            
-     
-    if df_big.empty == False:    
-        # completed experiments    
-        comps.append(exp[0:2])                
-        # scale the data    
+
+
+    if df_big.empty == False:
+        # completed experiments
+        comps.append(exp[0:2])
+        # scale the data
         df_scale = df_big.copy()
         trans = RobustScaler()# works better with outliers
-        df_scale[features] = trans.fit_transform(df_scale[features].values)  
-        
-        
+        df_scale[features] = trans.fit_transform(df_scale[features].values)
+
+
         # split into training and testing
         df_scale['is_train'] = np.random.uniform(0, 1, len(df_scale)) <= .80
         # split into training and testing
         train, test = df_scale[df_scale['is_train']==True], df_scale[df_scale['is_train']==False]
-        
+
         #rfor = RandomForestClassifier(n_jobs=2, random_state=0, max_depth = 5, max_features = 10, n_estimators = 200)
         #rfor.fit(train[features], train[pred_str])
 
@@ -142,23 +142,23 @@ for exp in exps:
              "gamma"            : [0.1, 0.2],
              "colsample_bytree" : [0.4, 0.5]
              }
-        
+
         grid_search = GridSearchCV(clf,
                             parameters, n_jobs=-1,
                             scoring="neg_log_loss",
                             cv=3)
-        
+
         grid_search.fit(train[features], train[pred_str])
         rfor = grid_search.best_estimator_
-        
+
         preds_for = rfor.predict(test[features])
-               
+
         cm = pd.crosstab(test[pred_str], preds_for, rownames=['Actual'], colnames=['Predicted'])
         print(cm)
-        
+
         mets = metrics.classification_report(test[pred_str], preds_for, digits=3, output_dict=True)
         print(mets)
-        
+
         cl_txt = ''
         # list of rec_cl0 rec_cl0
         cli=0
@@ -170,12 +170,12 @@ for exp in exps:
                 rec = scor['recall']
                 cl_txt = cl_txt + str(round(prec, 4)) + ' ' + str(round(rec, 4)) + ' '
             cli=cli+1
-            
-        
+
+
         # calculate model success, test, predictions
-        accur = metrics.accuracy_score(test[pred_str], preds_for) 
+        accur = metrics.accuracy_score(test[pred_str], preds_for)
         print("Accuracy:", accur)
-        
+
         outcomes = df_scale[pred_str].values.tolist()
         outs = set(outcomes)
         tot = len(outcomes)
@@ -183,24 +183,24 @@ for exp in exps:
         for out in outs:
             num = outcomes.count(out)
             rats.append(num/tot)
-           
+
         rand = sum(rats)/len(rats)
 
-        accur_str = accur_str+str(ei1)+' '+str(ei2max)+' '+str(clnum)+' '+str(round(accur, 4))+' '+str(round(rand, 4))+' '+cl_txt+"\n"  
+        accur_str = accur_str+str(ei1)+' '+str(ei2max)+' '+str(clnum)+' '+str(round(accur, 4))+' '+str(round(rand, 4))+' '+cl_txt+"\n"
         print(accur_str)
-        
+
         if get_imp:
             imps_str = "property_name property component importance confidence\n"
             save_txt = accur_txt.replace('accurs', exp[0:2]+'_imp')
-        
+
             shap_str = "property_name feat_num property component SHAP\n"
             shap_txt = accur_txt.replace('accurs', exp[0:2]+'_SHAP')
-        
+
             shap_vals = shap.TreeExplainer(rfor).shap_values(train[features])
-            
+
             shap.summary_plot(shap_vals, train[features], plot_type="bar")
             shap_comb = shap_vals[0].copy()
-                       
+
             if clnum==2 or len(shap_vals)==len(features):
                 print("only one value reported per class")
                 shap_comb = shap_vals.transpose()
@@ -210,59 +210,59 @@ for exp in exps:
                     shap0 = shap_vals[0]
                     shap1 = shap_vals[1]
                     shap2 = shap_vals[2]
-                    
+
                     for i in range(len(shap0)):
                         for j in range(len(shap0[0])):
                             shap_comb[i][j] = abs(shap0[i][j])+abs(shap1[i][j])+abs(shap2[i][j])
-                    
+
                     shap_comb = shap_comb.transpose()
                 else:
                     shap0 = shap_vals[0]
                     shap1 = shap_vals[1]
                     shap2 = shap_vals[2]
                     shap3 = shap_vals[3]
-                    
+
                     for i in range(len(shap0)):
                         for j in range(len(shap0[0])):
                             shap_comb[i][j] = abs(shap0[i][j])+abs(shap1[i][j])+abs(shap2[i][j])+abs(shap3[i][j])
-                      
-                    shap_comb = shap_comb.transpose()    
-            
+
+                    shap_comb = shap_comb.transpose()
+
             #if isinstance(shap_comb[0], list):
                 # row=sample
                 # col=features
-                
+
             # originally samples x features, and now features x samples
-    
+
             shap_mean = []
-            
+
             num_f = len(shap_comb)
             print("should be 9: ", num_f)
-            for fi in range(len(shap_comb)):              
+            for fi in range(len(shap_comb)):
                 vabs = abs(shap_comb[fi])
-                v_mean = statistics.mean(vabs)       
+                v_mean = statistics.mean(vabs)
                 shap_mean.append(v_mean)
-                
-            
-            
+
+
+
             shapl = list(zip(features, shap_mean))
             #recur = list(zip(features, recur_bin, recur_rank))
             shapl.sort(key=lambda tup: tup[1], reverse=True)
-        
+
             featn = len(features)
             imp = list(zip(features, rfor.feature_importances_))
             imp.sort(key=lambda tup: tup[1], reverse=True)
-        
+
             strc= ''
             for ft in imp:
                 fil = ft[0]
                 lis = fil.split('_')
-        
-        
+
+
                 if len(lis)==2:
                     comp = lis[0]
                     val = lis[1]
-                    
+
                     vs = 0
                     i=0
                     while i < len(vals):
@@ -270,7 +270,7 @@ for exp in exps:
                             vs = str(i)
                             i=len(vals)
                         i=i+1
-                    
+
                     if "dn" in comp:
                         cms= '1'
                     elif "dp" in comp:
@@ -285,33 +285,33 @@ for exp in exps:
                             cms = '-1'
                     else:
                         vs = '-1'
-                        cms = '0'  
+                        cms = '0'
                         if "delsig" in fil:
                             cms = '-1'
-                        
-                    
+
+
                 props = vs+' '+cms
-           
+
                 strc = strc+ft[0]+' '+props+' '+str(ft[1])+"\n"
-        
+
             imps_str = imps_str+strc
-                
+
             print(save_txt)
             print(imps_str)
             f= open(save_txt, "w")
             f.write(imps_str)
             f.close()
-            
+
             strc= ''
             for ft in shapl:
                 fil = ft[0]
                 lis = fil.split('_')
-        
-        
+
+
                 if len(lis)==2:
                     comp = lis[0]
                     val = lis[1]
-                    
+
                     vs = 0
                     i=0
                     while i < len(vals):
@@ -319,7 +319,7 @@ for exp in exps:
                             vs = str(i)
                             i=len(vals)
                         i=i+1
-                    
+
                     if "dn" in comp:
                         cms= '1'
                     elif "dp" in comp:
@@ -334,21 +334,21 @@ for exp in exps:
                             cms = '-1'
                     else:
                         vs = '-1'
-                        cms = '0'  
+                        cms = '0'
                         if "delsig" in fil:
                             cms = '-1'
-                            
+
                 props = vs+' '+cms
-          
+
                 strc = strc+ft[0]+' '+props+' '+str(ft[1])+"\n"
-        
+
             shap_str = shap_str+strc
             print(shap_txt)
             print(shap_str)
             f= open(shap_txt, "w")
             f.write(shap_str)
             f.close()
-        
+
 
     ei=ei+1
 
